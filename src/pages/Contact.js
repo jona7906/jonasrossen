944a0ce7479e559165e8c125fbb659eff3b968e8
JonasRossen <jona7906@stud.kea.dk>
2024-02-01 19:59:10 +0100

    New contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,10 @@
-import ContactSVG from "../svgs/icon_contact.svg";
-import ContactButton from "../components/ContactButton";
-
+import React, { useState } from "react";
+import ContactForm from "../components/ContactForm";
+import EmailSvg from "../svgs/icon_email.svg";
+import ExitButton from "../svgs/exit_button.svg";
 function Contact(props) {
+  const [showContactForm, setShowContactForm] = useState(false);
+
   if (!props) {
     return null;
   }
@@ -10,24 +13,40 @@ function Contact(props) {
     (intro) => intro.introduction_name === "contact"
   );
 
+  const toggleContactForm = () => {
+    setShowContactForm(!showContactForm);
+  };
+
+  const handleCloseContactForm = () => {
+    setShowContactForm(false);
+  };
+
   return (
     <div className="Internship">
       <div className="internship_section_01">
         <div className="icon_wrapper_01">
-          <img src={ContactSVG} alt="contact icon"></img>
+          <img src={EmailSvg} alt="email icon"></img>
         </div>
         <div className="text_wrapper_01">
           <h1>{contact_intro.introduction_title}</h1>
           {contact_intro.introduction_text?.map((text, index) => {
-            if (index <= 1) return <p>{text}</p>;
-            else return null;
+            return <p key={index}>{text}</p>;
           })}
 
           <div className="contact_wrapper">
-            {contact_intro.introduction_text?.map((text, index) => {
-              if (index > 1) return <ContactButton contact_text={text} />;
-              else return null;
-            })}
+            <button onClick={toggleContactForm}>
+              <p>Contact</p> <img src={EmailSvg} alt="email icon" />
+            </button>
+            {showContactForm && (
+              <div className="contact_form_wrapper">
+                <div className="form_wrapper">
+                  <button onClick={handleCloseContactForm}>
+                    <img src={ExitButton} alt="exit" />
+                  </button>
+                  <ContactForm />
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
